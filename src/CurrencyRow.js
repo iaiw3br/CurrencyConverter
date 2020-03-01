d61a1bfaf166a1c05a2ae59c96d5c6e6498ede7b
iaiw3br <iaiw3br@gmail.com>
2020-03-01 21:42:02 +0300

    add input value amount

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,24 +1,13 @@
 import React from "react";
 
-export default function CurrencyRow({currencies, selectCurrency, onChangeCurrency}) {
+export default function CurrencyRow({currencies, selectCurrency, onChangeCurrency, amount, onChangeAmount}) {
     return (
         <div>
-            <input type="number" className='input'/>
-            <select
-                value={selectCurrency}
-                onChange={onChangeCurrency}
-            >
-                {
-                    currencies.map(currency => (
-                        <option
-                            value={currency}
-                            key={currency}
-                        >
-                            {currency}
-                        </option>
-                    ))
-                }
-
+            <input type="number" className="input" value={amount} onChange={onChangeAmount}/>
+            <select value={selectCurrency} onChange={onChangeCurrency}>
+                {currencies.map(currency => (
+                    <option value={currency} key={currency}>{currency}</option>
+                ))}
             </select>
         </div>
     );
